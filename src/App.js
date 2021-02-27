e6bb60c141a6e6f71ae220f0e2a3e77d2d008b07
Isaac Amoafo <Isaac@PSL-AMOAFO>
2021-02-27 14:13:50 +0000

    worked on collection preview and collection item

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import Homepage from './pages/homepage/homepage.component';
 import {Route, Switch} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.components.jsx';
 
 import './App.css';
 
-const HatsPage = () =>(
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+
+
+
 
 function App() {
   return (
     <div >
       <Switch>
     <Route exact path='/' component={Homepage}/>
-    <Route path='/hats' component={HatsPage}/>
+    <Route path='/shop' component={ShopPage}/>
     </Switch>
     </div>
   );
