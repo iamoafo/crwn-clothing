1d9e5a7f9602a92d7d05f1ed106ef49432e3b64d
Isaac Amoafo <Isaac@PSL-AMOAFO>
2021-03-24 17:09:52 +0000

    moved our directory and shop data into its own reducer , created respective selectors and updated corresponding components with new redux flow for directory.shop

--- src/pages/shop/shop.components.jsx
+++ src/pages/shop/shop.components.jsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import SHOP_DATA from './shop.data.js';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx'
+import CollectionsOverview from '../../components/collections-overView/collections-overview.component'
+import {Route} from 'react-router-dom';
+import CollectionPage from "../collection/collection.component";
 
-class ShopPage extends React.Component{
-    constructor(props){
-        super(props);
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-        }
 
-        render(){
-            return(<div className='shop-page'>
-                {this.state.collections.map(({id, ...otherCollectionProps}) =>(
-                     <CollectionPreview key={id} {...otherCollectionProps}/>
-                ))}
-                </div>);
-        }
+const ShopPage = ({match})=>(
+   
+<div className='shop-page'>
+<Route exact path={`${match.path}`} component={CollectionsOverview} />
+<Route path={`${match.path}/:collectionId`} component={CollectionPage} />          
+ </div>
            
-        
-    
-}
+);
 
-export default ShopPage;
+
+
+export default (ShopPage);
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,16 @@ import { createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 import { composeWithDevTools } from "redux-devtools-extension"
+import {persistStore } from 'redux-persist'
 
 //const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const middlewares = [logger];
 
-const store = createStore(
+export const store = createStore(
     rootReducer,composeWithDevTools(applyMiddleware(...middlewares))
     );
 
-export default store;
+export const persistor = persistStore(store);
+
+export default {store,persistor};
